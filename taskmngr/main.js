69ca92b40fa32d0ed49e977f4d27ee5c7289211a
matveyich <matveyich@gmail.com>
2013-06-27 21:00:43 +0300

    Update main.js

--- taskmngr/main.js
+++ taskmngr/main.js
@@ -4,6 +4,36 @@ function setCurrentDate() {
 	dateContainer.innerHTML = curDate.getDate() + "/" + curDate.getMonth() + "/" + curDate.getFullYear();
 
 }
+
+function clearTasks() {
+	var tasks = window.document.getElementById("tasks");
+	tasks.innerHTML("");
+}
+
+function fillTasks(tasks) {
+
+}
+
+function pushTask(task) {
+
+}
+
+var tasks = [
+	{
+		title: "Строка управления",
+		dueDate: "24.06.2013",
+		taskBody: "Рассмотреть use cases работы элемента в контексте бизнес процессов.",
+		taskFullText: "Рассмотреть use cases работы элемента в контексте бизнес процессов."
+	},
+	{
+		title: "CTI-панель",
+		dueDate: "24.06.2013",
+		taskBody: "Выслать участникам на проработку и на проработку дизайна отдельных элементов (статусы, клавиатура)",
+		taskFullText: "Выслать участникам на проработку и на проработку дизайна отдельных элементов (статусы, клавиатура)"
+	}		
+	];
+
 window.onload = function() {
 	setCurrentDate();
+	clearTasks();
 }
