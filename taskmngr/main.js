81fe188de6e3abd649a6373e3e6daca49fb1e0e6
matveyich <matveyich@gmail.com>
2013-06-27 20:38:05 +0300

    Update main.js

--- taskmngr/main.js
+++ taskmngr/main.js
@@ -1,6 +1,9 @@
-window.onload = function() {
-  var dateContainer = window.document.getElementById("date");
+function setCurrentDate() {
+	var dateContainer = window.document.getElementById("date");
 	var curDate = new Date();
-	
 	dateContainer.innerHTML = curDate.getDate() + "/" + curDate.getMonth() + "/" + curDate.getFullYear();
+
+}
+window.onload = function() {
+	setCurrentDate();
 }
